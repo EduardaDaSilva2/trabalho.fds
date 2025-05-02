1b7a889a2061d07458687a8dee0d5e20e9da832f
Eduarda Santos Ribeiro da Silva <kiranyukki@gmail.com>
2025-05-02 14:09:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('mensagemBtn').addEventListener('click', function() {
+    const mensagemContainer = document.getElementById('mensagemContainer');
+    mensagemContainer.innerHTML = '<p>"Acredite em si mesmo e você será imparável."</p>';
+    mensagemContainer.style.display = 'block';
+});
