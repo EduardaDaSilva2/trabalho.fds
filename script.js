f50f7387b5390e448d226409b07fe199b4ff77dd
Eduarda Santos Ribeiro da Silva <kiranyukki@gmail.com>
2025-05-02 14:55:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('mensagemBtn').addEventListener('click', function() {
+document.getElementById('mensagemBotão').addEventListener('click', function() {
     const mensagemContainer = document.getElementById('mensagemContainer');
     mensagemContainer.innerHTML = '<p>"Acredite em si mesmo e você será imparável."</p>';
     mensagemContainer.style.display = 'block';
